e1ba08e5c421fb939730b7dd233b3640245d50fa
DanglingPointer <mikail.vasilyev@gmail.com>
2020-06-14 12:20:07 +0200

    WorkerPool: Removed unnecessary copying

--- tests/async_test.cpp
+++ tests/async_test.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include <deque>
 #include <functional>
+#include <future>
 #include <limits>
 #include <optional>
 #include <unordered_set>
@@ -984,6 +985,74 @@ TEST_F(WorkerPoolFixture, worker_catches_exceptions)
 
    const char loglineStart[] = "Uncaught exception in thread";
    EXPECT_STREQ(loglineStart, loglines.back().substr(0, sizeof(loglineStart) - 1).c_str());
+}
+
+TEST_F(WorkerPoolFixture, worker_doesnt_do_unnecessary_copies)
+{
+   struct Counter
+   {
+      size_t * copyCount;
+      size_t * moveCount;
+      Counter(size_t * copyCount, size_t * moveCount)
+          : copyCount(copyCount)
+          , moveCount(moveCount)
+      {}
+      Counter(const Counter & rhs)
+          : copyCount(rhs.copyCount)
+          , moveCount(rhs.moveCount)
+      {
+         ++(*copyCount);
+      }
+      Counter(Counter && rhs)
+          : copyCount(rhs.copyCount)
+          , moveCount(rhs.moveCount)
+      {
+         ++(*moveCount);
+      }
+      void signalFinished(std::promise<void> & p)
+      {
+         p.set_value();
+      }
+   };
+
+   ThreadPool p(GetLogger(), GetTime());
+   std::this_thread::sleep_for(500ms);
+
+   {
+      size_t copyCount = 0;
+      size_t moveCount = 0;
+      std::promise<void> finished;
+
+      Counter c(&copyCount, &moveCount);
+      auto future = finished.get_future();
+
+      p.Execute([&finished, c = std::move(c)]() mutable {
+         c.signalFinished(finished);
+      });
+
+      auto status = future.wait_for(5s);
+      EXPECT_EQ(std::future_status::ready, status);
+      EXPECT_EQ(0U, copyCount);
+      EXPECT_GE(3U, moveCount);
+   }
+   {
+      size_t copyCount = 0;
+      size_t moveCount = 0;
+      std::promise<void> finished;
+
+      Counter c(&copyCount, &moveCount);
+      auto future = finished.get_future();
+      auto task = [&finished, c=std::move(c)]() mutable {
+         c.signalFinished(finished);
+      };
+
+      p.Execute(task);
+
+      auto status = future.wait_for(5s);
+      EXPECT_EQ(std::future_status::ready, status);
+      EXPECT_EQ(1U, copyCount);
+      EXPECT_GE(2U, moveCount);
+   }
 }
 
 } // namespace
